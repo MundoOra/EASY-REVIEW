ab801cdd3d24885889fb23fdc7a99db75d7e96bd
MundoOra <mundoorar@gmail.com>
2022-11-24 01:22:18 -0600

    Initial commit

---
+++ src/public/workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'C:/Users/mundo/Desktop/EASY REVIEW/src/public',
+	globPatterns: [
+		'**/*.{gif,jpg,js,html,css,png,json,mp3}'
+	],
+	swDest: 'C:/Users/mundo/Desktop/EASY REVIEW/src/public/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
